c34964907e6a32abf99dbd6df5789c36ec09007a
Paul-ih <paul.parant@gmail.com>
2021-05-26 20:20:21 +0200

    creation user OK mais pas connexion

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
 exports.signup = (req, res, next) => {
@@ -11,7 +12,7 @@ exports.signup = (req, res, next) => {
       });
       user
         .save()
-        .then(() => res.status(201).json({ message: "Utilisateur créé." }))
+        .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
         .catch((error) => res.status(400).json({ error }));
     })
     .catch((error) => res.status(500).json({ error }));
@@ -21,17 +22,20 @@ exports.login = (req, res, next) => {
   User.findOne({ email: req.body.email })
     .then((user) => {
       if (!user) {
-        return res.status(401).json({ error: "Utilisateur non trouvé." });
+        return res.status(401).json({ error: "Utilisateur non trouvé" });
       }
       bcrypt
         .compare(req.body.password, user.password)
         .then((valid) => {
           if (!valid) {
-            return res.status(401).json({ error: "Mot de passe incorrect." });
+            return res.status(401).json({ error: "Mot de passe incorrect" });
           }
           res.status(200).json({
             userId: user._id,
-            token: "TOKEN",
+            token: jwt.sign({ userId: user._id },
+                'RANDOM_TOKEN_SECRET',
+                {expiresIn: '24h'}
+                ),
           });
         })
         .catch((error) => res.status(500).json({ error }));
