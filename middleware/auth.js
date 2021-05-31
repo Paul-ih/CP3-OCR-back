65db2a1f73180ac6c8cffaf33fd555c8e43141c0
Paul-ih <paul.parant@gmail.com>
2021-05-31 19:13:09 +0200

    middleware auth NON

---
+++ middleware/auth.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decodedToken = jwt.verify(
+      token,
+      "RANDOM_TOKEN_SECRET_$2b$10$505aWNvcnc10vMBAz2/ZpONj/z4P9CsajVZ3KjkXdFTdNKJOfQ/pu"
+    );
+    const userId = decodedToken.userId;
+    if (req.body.userId && req.body.userId !== userId) {
+      throw "Invalid user ID";
+    } else {
+      next();
+    }
+  } catch {
+    res.status(401).json({
+      error: new Error("Invalid request!"),
+    });
+  }
+};
--- routes/stuff.js
+++ routes/stuff.js
@@ -4,12 +4,16 @@ const router = express.Router();
 // RENVOI VERS CONTROLLERS
 const stuffCtrl = require("../controllers/stuff");
 
+// MIDDLEWARE D'AUTHENTIFICATION
+const auth = require("../middleware/auth");
+
 // CRÉER, MODIFIER, SUPPRIMER, LIRE 1, LIRE TOUS OBJETS
 
-router.post("/", stuffCtrl.createThing);
-router.put("/:id", stuffCtrl.modifyThing);
-router.delete("/:id", stuffCtrl.deleteThing);
-router.get("/:id", stuffCtrl.getOneThing);
-router.get("/", stuffCtrl.getAllThings);
+router.post("/", auth, stuffCtrl.createThing);
+router.put("/:id", auth, stuffCtrl.modifyThing);
+router.delete("/:id", auth, stuffCtrl.deleteThing);
+// ! auth vérification aussi pour le get, pas nécessaire ?
+router.get("/:id", auth, stuffCtrl.getOneThing);
+router.get("/", auth, stuffCtrl.getAllThings);
 
 module.exports = router;
